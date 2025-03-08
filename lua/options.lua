bb2edaa482c3fcab20309646c74f6c3c31c56e57
luan441 <vloid@proton.me>
2025-03-08 10:19:53 -0300

    feat: typescript-tools

--- lua/options.lua
+++ lua/options.lua
@@ -61,4 +61,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.cmd 'set expandtab'
+vim.cmd 'set tabstop=2'
+vim.cmd 'set softtabstop=2'
+vim.cmd 'set shiftwidth=2'
+
 -- vim: ts=2 sts=2 sw=2 et
