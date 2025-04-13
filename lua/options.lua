8eea6ceb1dfd5d6fd11374e06612ca8c2f36c30c
Luan Germano <59399061+luan441@users.noreply.github.com>
2025-04-13 20:14:13 -0300

    feat: options indent

--- lua/options.lua
+++ lua/options.lua
@@ -66,4 +66,10 @@ vim.opt.scrolloff = 10
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
+-- Indent
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 -- vim: ts=2 sts=2 sw=2 et
